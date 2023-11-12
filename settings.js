1000b6399d82280125b569898cf55281ed91fa38
Waqasahmad9300 <108908697+Waqasahmad9300@users.noreply.github.com>
2023-11-12 14:04:58 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,31 +36,41 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['94711421243'] //ur owner number
-global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ ᴀʟᴘʜᴀ" //ur owner name
-global.ytname = "YT: TGM Arcade" //ur yt chanel name
+global.vcardowner = ['03077889300'] //ur owner number
+global.ownername = "Kashii Lover" //ur owner name
+global.ytname = "https://youtube.com/@FFKashii?si=k1Wer1M5LLmgroWz" //ur yt chanel name
 global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
-global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
+global.location = "Sri Lanka, pakistan, Nikaweratiya" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
-global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
-global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
+global.owner = ['03077889300']
+global.ownertag = '03077889300' //ur tag number
+global.botnumber = '03077889300' //Add your Bot Number For Log
+global.botname = 'kashii Lover' //ur bot name
+global.ig = "https://youtube.com/@FFKashii?si=k1Wer1M5LLmgroWz" //your theme url which will be displayed on whatsapp
+global.myweb = "https://youtube.com/@FFKashii?si=k1Wer1M5LLmgroWz" //ur website to be displayed
 global.sc = 'https://github.com/darkalphaxteam' //script link
-global.linkz = 'https://youtu.be/uzMbW5v9QLE'
+global.linkz = 'https://youtube.com/@FFKashii?si=k1Wer1M5LLmgroWz'
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "📩" //ur theme emoji
-global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
-global.author = "DARK ALPHA" //ur sticker watermark author
-global.footer = "® ᴄʏʙᴇʀ-x ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ"
+global.packname = "KAshii Love Mishii" //ur sticker watermark packname
+global.author = "mishii" //ur sticker watermark author
+global.footer = "® Kashii love Mishii"
 global.aliveimg ="https://telegra.ph/file/38c052d313ccf14d80550.jpg"
 global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
 global.welcomeimg= "https://telegra.ph/file/844812c77704231c08a90.jpg"
 global.gbyeimg = "https://telegra.ph/file/844812c77704231c08a90.jpg"
-global.alivemsg = "ඔබ සතුව නැති දේ ලබා ගැනීමේ පහසුම ක්‍රමය  ඇගයීමයි.. හැකි සෑම විටම තව කෙනෙක්ව ඇගයීමට උත්සහ කරන්න. එය ඔබේ මානසික සංවර්ධිත බවේ සලකුණකි..\nසංස්කරණය කළ පරිවර්තනයකි...\n@best_m_otivation\n#lifetips\n#psychology\n#questions"
+global.alivemsg = "Kashii 💕 Mishii 
+┏━━━━ ﷽ ━━━━┓
+🖤Kashii 💕 Mishii
+┗━━━━ ﷽ ━━━━┛
+*⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎*
+ *╰─ ➤s̳ᴱ̳ɴ̳ᴅ̳ ʙ̳ʏ̳⭏*
+ *╭───────────────*
+ *̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊╰─☞︎︎︎●⃝Kashii💞Mishii*
+*▬▭▬▭▬▭▬▭▬▭▬▭*
+ 🖤      ✍🏻ㅤ    📩       📤 
+ *ˡᶦᵏᵉ   ᶜᵒᵐᵐᵉⁿᵗ    ˢᵃᵛᵉ      ˢʰᵃʳᵉ*"
 
 //BOT MEDIAS
 global.thum = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
